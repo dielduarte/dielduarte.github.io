da2866067aaa562a2da154ef293e018ae8a03f1d
dielduarte <magdiel.kesller@gmail.com>
2023-08-31 00:04:19 -0300

    improving plugin and og images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,85 @@
 import { defineConfig } from 'astro/config';
+import slugify from 'slugify';
 
 import tailwind from "@astrojs/tailwind";
 import generateOgImages from './src/integrations/generate-og-images'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), generateOgImages()],
+  integrations: [tailwind(), generateOgImages({
+    filterRoutes: (route) => route.component.endsWith('.md'),
+    getImagePath: (attributes, options) => ({
+      url: `./${slugify(attributes.slug.replace('/', '-'))}.png`,
+      base: options.dir + 'images/'
+    }),
+    getSatoriConfig: ({title}) => {
+      //@TODO make it an option
+      const robotoArrayBuffer = fs.readFileSync('./public/PressStart2P-Regular.ttf')
+
+      return {
+        content: {
+            type: 'div',
+                props: {
+                children: [
+                  {
+                    type: 'div',
+                    props: {
+                      style: {
+                        width: '80%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        textAlign: 'center',
+                        alignItems: 'center',
+                      },
+                      children: [
+                        {
+                          type: 'p',
+                          props: {
+                            style: { fontSize: 24 },
+                            children: 'Diel Duarte',
+                          },
+                        },
+                        {
+                          type: 'p',
+                          props: {
+                            style: { fontSize: 30, lineHeight: 1.5 },
+                            children: title,
+                          },
+                        },
+                      ],
+                    },
+                  },
+                ],
+                style: {
+                  background: 'black',
+                  width: '100%',
+                  height: '100%',
+                  display: 'flex',
+                  textAlign: 'center',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  color: 'white',
+                  backgroundSize: '100px 100px',
+                  padding: '30px'
+                },
+              },
+        },
+        options:{
+          width: 1200,
+          height: 600,
+          fonts: [
+            {
+              name: 'pressStart',
+              data: robotoArrayBuffer,
+              weight: 400,
+              style: 'normal',
+            },
+          ],
+        }
+      }
+    }
+  })],
   site: 'https://dielduarte.dev',
   redirects: {
     '/': '/blog/en',
--- src/integrations/generate-og-images.js
+++ src/integrations/generate-og-images.js
@@ -1,95 +1,31 @@
 import satori from 'satori';
 import fs from 'node:fs';
+import worker from 'node:worker_threads';
 import { writeFile } from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
 import fm from 'front-matter'
 import svg2png from "svg2png"
-import slugify from 'slugify';
 
-export default () => ({
+export default ({getSatoriConfig, filterRoutes, getImagePath}) => ({
   name: 'generate-og-images',
   hooks: {
     'astro:build:done': async (options) => {
-      // TODO make it an option
-      const articles = options.routes.filter((route) => route.component.endsWith('.md'))
+      const articles = options.routes.filter(filterRoutes)
 
-      
-      //@TODO make it an option
-      const robotoArrayBuffer = fs.readFileSync('./public/PressStart2P-Regular.ttf')
+      await Promise.all(articles.map((article) => {
+        return new Promise(async (resolve) => {
+          const content = fs.readFileSync(article.component, 'utf-8')
+          const {attributes} = fm(content)
+          const config = getSatoriConfig(attributes)
 
-      const getSvg = (title) => satori(
-        {
-          type: 'div',
-              props: {
-              children: [
-                {
-                  type: 'div',
-                  props: {
-                    style: {
-                      width: '80%',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      textAlign: 'center',
-                      alignItems: 'center',
-                    },
-                    children: [
-                      {
-                        type: 'p',
-                        props: {
-                          style: { fontSize: 24 },
-                          children: 'Diel Duarte',
-                        },
-                      },
-                      {
-                        type: 'p',
-                        props: {
-                          style: { fontSize: 30, lineHeight: 1.5 },
-                          children: title,
-                        },
-                      },
-                    ],
-                  },
-                },
-              ],
-              style: {
-                background: 'black',
-                width: '100%',
-                height: '100%',
-                display: 'flex',
-                textAlign: 'center',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'column',
-                color: 'white',
-                backgroundSize: '100px 100px',
-                padding: '30px'
-              },
-            },
-        },
-        {
-          width: 1200,
-          height: 600,
-          fonts: [
-            {
-              name: 'pressStart',
-              data: robotoArrayBuffer,
-              weight: 400,
-              style: 'normal',
-            },
-          ],
-        },
-      )
-
-      for (const article of articles) {
-        const content = fs.readFileSync(article.component, 'utf-8')
-        const {attributes} = fm(content)
-
-        const svg = await getSvg(attributes.title)
-        const img = await svg2png.sync(svg)
-        // TODO make it an option
-        const outFile = fileURLToPath(new URL(`./${slugify(attributes.slug.replace('/', '-'))}.png`, options.dir + 'images/'));
-        await writeFile(outFile, img);
-      }
+          const svg = await satori(config.content, config.options)
+          const img = await svg2png.sync(svg)
+          const {url, base} = getImagePath(attributes, options)
+          const filePath = fileURLToPath(new URL(url, base));
+          await writeFile(filePath, img);
+          resolve()
+        })
+      }))
     }
   }
 })
