19471707dd3e2ee17e6d4b143583f860cc303434
dielduarte <magdiel.kesller@gmail.com>
2024-05-26 21:17:10 -0300

    make rss generate with content

--- src/pages/blog/pt-br/rss.xml.js
+++ src/pages/blog/pt-br/rss.xml.js
@@ -1,6 +1,16 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 
 export async function GET(context) {
+  const posts = 
+    await import.meta.glob('./**/*.{md,mdx}', { eager: true })
+
+  const items = Object.values(posts).map((post) => ({
+      link: post.url,
+      content: sanitizeHtml(post.compiledContent()),
+      ...post.frontmatter,
+    }))
+
   return rss({
     // `<title>` field in output xml
     title: 'Diel Duarte Blog',
@@ -11,9 +21,7 @@ export async function GET(context) {
     site: context.site,
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./**/*.{md,mdx}'),
-    ),
+    items,
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
