9a3a1c59e7544803cbaec7e4a801cf75e314113c
dielduarte <magdiel.kesller@gmail.com>
2025-02-01 22:19:11 -0300

    finishing the refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import slugify from 'slugify';
 
 import tailwind from "@astrojs/tailwind";
 import generateOgImages from './src/integrations/generate-og-images'
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), generateOgImages({
+  integrations: [react(),tailwind(), generateOgImages({
     filterRoutes: (route) => route.component.endsWith('.md'),
     getImagePath: (attributes, options) => ({
       url: `./${slugify(attributes.slug.replace('/', '-'))}.png`,
